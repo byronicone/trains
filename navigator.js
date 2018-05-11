22708d08ceeabf091accfe41000a861af48521ad
byronicone <byron.e.barnes@gmail.com>
2018-05-11 15:00:07 -0500

    All tests are passing, some refactoring may be done later.

--- navigator.js
+++ navigator.js
@@ -4,10 +4,9 @@ var cityMap = new Map();
 exports.loadMap = (...graph) => {
   graph.map( (route) => {
     route = route.split('');
-    console.log(route);
     city = route[0];
     let neighbor = route[1];
-    let distance = route[2];
+    let distance = parseInt(route[2]);
     let neighborMap = cityMap.has(city) ? cityMap.get(city) : new Map();
     neighborMap.set(neighbor, distance);
     cityMap.set(city, neighborMap);
@@ -16,18 +15,75 @@ exports.loadMap = (...graph) => {
 }
 
 /*Compute the distance between two or more cities*/
-exports.computeDistance = (cities)  => {
+exports.computeDistance = (...cities)  => {
+  //find the list of neighbors of the first city
+  let first = cities[0];
+  if(cities.length > 1){
+    //attempt to calculate distance to next city
+    let next = cities[1];
+    let neighborMap = cityMap.get(first)
+    let distance = neighborMap.get(next);
+    if(distance === undefined){
+      return 'NO SUCH ROUTE';
+    }
+    else{
+      //if more cities exist, compute again
+      cities.shift();
+      let add = exports.computeDistance(...cities);
+      if(typeof add == 'number'){
+        return distance+add;
+      }
+      else{
+        //if type is not a number, we are getting an error which should be returned
+        return add;
+      }
+    }
+  }
+  else{
+    //we are done, and this node is navigating to itself.
+    return 0;
+  }
 }
 
-/*Compute the number of trips between two cities
- * optionally require a maximum or exact number of stops*/
-exports.findNumTrips = (cities) => {
+/* Get the trips between two cities
+ * with a maximum or exact number of stops*/
+exports.findTrips = (departure, arrival, maxStops, exact, maxDistance, currentStop) => {
+  currentStop = currentStop ? currentStop+1 : 1;
+  maxStops = maxStops ? maxStops : 10;
+  let routes=[];
+
+  if(currentStop > maxStops){
+    return [];
+  }
+
+  //get all of the direct connections from the departure city
+  let connections = cityMap.get(departure);
+
+  for(let [city, distance] of connections){
+    if(currentStop!=maxStops){
+      let validRoutes = exports.findTrips(city, arrival, maxStops, exact, maxDistance, currentStop);
+      if(validRoutes){
+        validRoutes.forEach( function (val, i) {
+        prefix = currentStop == 1 ? departure + city : city;
+        let totalDistance = distance + val[1];
+        if(!maxDistance || totalDistance <= maxDistance){
+          routes.push([prefix+val[0], distance+val[1]]);
+        }
+        });
+      }
+    }
+    if((city === arrival) && (!exact || (exact && currentStop == maxStops))){
+      if(!maxDistance || distance <= maxDistance){
+        routes.push([city, distance]);
+      }
+    }
+  }
+  return routes;
 }
 
 /*Calculate the shortest distance between two cities*/
-exports.getMinDistance = (cities) => {
+exports.getMinTrip = (departure, arrival) => {
+  let trips = exports.findTrips(departure, arrival, 5, false);
+  trips.sort( function(a,b){ return a[1]-b[1] });
+  return trips[0];
 }
-
-/*Determine the number of unique routes between two cities
- * optionally require a maximum distance */
-
--- test/trains-test.js
+++ test/trains-test.js
@@ -4,25 +4,48 @@ const nav= require('../navigator');
 describe('trains', () => {
   //Graph AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7
   nav.loadMap('AB5','BC4','CD8','DC8','DE6','AD5','CE2','EB3','AE7');
+  it('returns 0 if only one city is passed', () => {
+    assert.equal(0,nav.computeDistance('A'));
+  })
   it('computes distance of route A-B-C', () => {
+    assert.equal(9,nav.computeDistance('A','B','C'));
   })
   it('computes distance of route A-D', () => {
+    assert.equal(5,nav.computeDistance('A','D'));
   })
   it('computes distance of route A-D-C', () => {
+    assert.equal(13,nav.computeDistance('A','D','C'));
   })
   it('computes distance of route A-E-B-C-D', () => {
+    assert.equal(22,nav.computeDistance('A','E','B','C','D'));
   })
   it('computes distance of route A-E-D', () => {
+    assert.equal('NO SUCH ROUTE',nav.computeDistance('A','E','D'));
   })
   it('finds number of trips from C to C with a max of 3 stops', () => {
+    let trips = nav.findTrips('C','C', 3);
+    console.log(trips);
+    assert.equal(trips.length,2);
   })
   it('finds number of trips from A to C with exactly 4 stops', () => {
+    let trips = nav.findTrips('A', 'C', 4, true);
+    console.log(trips);
+    assert.equal(trips.length, 3);
   })
   it('calculates shortest distance from A to C', () => {
+    let minTrip = nav.getMinTrip('A','C');
+    console.log(minTrip);
+    assert.equal(minTrip[1],9);
   })
   it('calculates shortest distance from B to B', () => {
+    let minTrip= nav.getMinTrip('B','B');
+    console.log(minTrip);
+    assert.equal(minTrip[1],9);
   })
   it('determines number of unique routes from C to C with a distance less than 30', () => {
+   let trips = nav.findTrips('C','C',10,false,30);
+    console.log(trips);
+    assert.equal(trips.length,9);
   })
 
 })
